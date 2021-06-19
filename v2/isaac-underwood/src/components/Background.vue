c30e7c70d113d2c42d3034f4a520bf1800ed3850
Isaac Underwood <isaacund220220@gmail.com>
2021-06-19 15:14:04 +1200

    Add background component and scripts

---
+++ v2/isaac-underwood/src/components/Background.vue
@@ -0,0 +1,28 @@
+<template>
+  <canvas id="animatedBackground"></canvas>
+</template>
+
+<script lang="ts">
+import animateBackground from "@/animation/background";
+import { defineComponent, onMounted } from "vue";
+
+export default defineComponent({
+  name: "Background",
+  setup() {
+    const bdIdName = "animatedBackground";
+
+    onMounted(() => {
+      animateBackground(bdIdName);
+    });
+  },
+});
+</script>
+
+<style lang="less">
+canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
