af9c1445c35edca2bf317815d82dd7303a74d48c
Isaac Underwood <isaacund220220@gmail.com>
2022-04-15 00:22:40 +1200

    Update portfolio

--- js/portfolio.js
+++
@@ -1,9 +0,0 @@
-new fullpage('#fullpage', {
-    licenseKey: '4EABA41D-FAE8499F-AEDA006E-104BDDC3',
-    //options here
-    scrollBar: true,
-    autoScrolling: true,
-    verticalCentered: true,
-    paddingTop: '63px',
-    fixedElements: 'nav, .navbar',
-});
